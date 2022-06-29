7f75a6d47832414eb089c8a269bd9d7a37a0cd32
RaazDk1992 <dhakal1992raaz@gmail.com>
2022-06-29 21:32:12 +0545

    authentication and new templates

--- plan/urls.py
+++ plan/urls.py
@@ -1,12 +1,15 @@
+from unicodedata import name
 from django.conf.urls import handler404, handler500
 from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.default, name="revhome"),
+    path('', views.default, name="default"),
+    path('validate', views.verification, name='validate'),
     path('reg/',views.registerPlan,name="registerplan"),
     path('addfy/',views.addFY,name='addFy'),
     path('addprojecttype/',views.addProjectType,name='projecttype'),
-    path('projectcategory/',views.addTypeOfProject,name='TypeOfProject')
+    path('projectcategory/',views.addTypeOfProject,name='TypeOfProject'),
+    path('admindashboard/',views.adminDashBoard,name='admindashboard')
 ]
--- plan/views.py
+++ plan/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth 
+from django.contrib.auth import authenticate,login
+
 from .forms import FYform,YojanaRegForm,ProjectTypesForms,TypeOfProjectForm
 from .models import FY, YojanaDetails
+
 # Create your views here.
+
+def adminDashBoard(request):
+    
+    return render(request,'pages\\admindashboard.html') 
+
+def default(request):
+    return render(request,'login\\index.html') 
+
+
+def verification(request):
+
+    if request.method == 'POST':
+        userName = request.POST['userName']
+        passWord = request.POST['passWord']
+        user = authenticate(request, username=userName, password=passWord)
+        
+        if user is not None:
+            login(request,user)
+           # 
+            return redirect('admindashboard')
+        else:
+            return redirect('default')
+
 def default(request):
     return render(request,'login\\index.html') 
 
